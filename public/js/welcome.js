86fdc97fbc6b2d112b3f9f7807ac620b762ae8f2
Cait Friedlander <caitfinchart@gmail.com>
2016-11-27 16:37:19 -0800

    make modal for instagif

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -20,3 +20,7 @@ $('#instagif').click(function (){
     })
 })
 
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
